8702ac1d91260f0476c5fb9c7f6b7a865c06cd1b
Shumpei Yamada <shumpei.yamada@salesforce.com>
2024-01-06 16:07:09 +0900

    mod

--- cartridges/sample_custom_stripe/cartridge/scripts/hooks/basketPaymentHooks.js
+++ cartridges/sample_custom_stripe/cartridge/scripts/hooks/basketPaymentHooks.js
@@ -1,38 +1,40 @@
 var Transaction = require('dw/system/Transaction');
 var service = require('~/cartridge/scripts/stripe/service');
+const Logger = require('dw/system/Logger');
 
 // eslint-disable-next-line no-unused-vars
 exports.beforePOST = function (basket, paymentInstrument) {
+    var instruments = basket.getPaymentInstruments('STRIPE').iterator();
+    if (instruments.hasNext()) {
+        basket.removePaymentInstrument(instruments.next());
+    }
     if (paymentInstrument.paymentMethodId === 'STRIPE') {
         Transaction.wrap(function () {
-            const prifile = request.session.customer.getProfile()
-            if (!prifile.custom.stripeCustomerId) {
-                // create stripe customer id
-                prifile.custom.stripeCustomerId = "dummy_stripe_customer_id"
+            const profile = request.session.customer.getProfile()
+            if (!profile.custom.stripeCustomerId) {
+                const customer = service.createCustomer(`${profile.lastName} ${profile.firstName}`, profile.email, profile.customerNo)
+                profile.custom.stripeCustomerId = customer.id
             }
-
-            let intentId = "dummy_intent_id"
-            if (paymentInstrument.c_stripePaymentMethodId) {
-                // create intent id with stripe payment method id
-            } else {
-                // create intent id without stripe payment method id
-            }
-            request.custom.stripeIntentId = intentId;
+            const intent = service.createPaymentIntent(basket.getTotalGrossPrice().getValue(), basket.getCurrencyCode().toLowerCase(), paymentInstrument.c_stripePyamentMethodId, profile.custom.stripeCustomerId)
+            request.custom.c_stripeIntentId = intent.id
+            request.custom.c_stripePyamentMethodId = intent.payment_method
+            request.custom.c_stripeClientSecret = intent.client_secret
         })
     }
 };
 
-exports.afterPOST = function (_order, _paymentInstrument) {
-    // Do nothing
-};
-
 // eslint-disable-next-line no-unused-vars
-exports.modifyPOSTResponse = function (basket, basketResponse, paymentInstrumentRequest) {
-    var addedPayment = basketResponse.paymentInstruments.toArray().filter(function (instr) {
-        return instr.paymentMethodId === "STRIPE";
-    })[0];
-    addedPayment.c_stripeIntentId = request.custom.stripeIntentId;
+exports.afterPOST = function (basket, paymentInstrument) {
+    Transaction.wrap(function () {
+        var instruments = basket.getPaymentInstruments('STRIPE').iterator();
+        if (instruments.hasNext()) {
+            var instrument = instruments.next();
+            instrument.custom.stripeIntentId = request.custom.c_stripeIntentId;
+            instrument.custom.stripePaymentMethodId = request.custom.c_stripePyamentMethodId;
+            instrument.custom.stripeClientSecret = request.custom.c_stripeClientSecret;
+            instrument.custom.willSaved = paymentInstrument.c_willSaved;
+        };
+    })
+}
 
-    // add Stripe payment methods
 
-};
--- cartridges/sample_custom_stripe/cartridge/scripts/stripe/service.js
+++ cartridges/sample_custom_stripe/cartridge/scripts/stripe/service.js
@@ -3,13 +3,14 @@
 var LocalServiceRegistry = require("dw/svc/LocalServiceRegistry");
 const Logger = require('dw/system/Logger');
 
-function getService(fn) {
+function _getService(fn) {
     return LocalServiceRegistry.createService(
         "stripe.service",
         {
             createRequest: fn,
             parseResponse: function (svc, client) {
                 try {
+                    Logger.debug('Response: {0} ;; Client: {1}', client.text, JSON.stringify(client));
                     return JSON.parse(client.text);
                 } catch (e) {
                     Logger.error('Unable to Authenticate. Error: {0} ;; Response: {1} ;; Client: {2}', e.message, client.text, JSON.stringify(client));
@@ -23,8 +24,8 @@ function getService(fn) {
     );
 }
 
-function getRetrieveServise() {
-    return getService(function (svc, endpoint) {
+function _getRetrieveServise() {
+    return _getService(function (svc, endpoint) {
         // https://stripe.com/docs/api/authentication
         const requestURL = svc.getURL() + endpoint
         const svcCredential = svc.getConfiguration().credential;
@@ -35,8 +36,8 @@ function getRetrieveServise() {
     })
 }
 
-function getOperationService() {
-    return getService(function (svc, endpoint, request) {
+function _getOperationService() {
+    return _getService(function (svc, endpoint, make) {
         // https://stripe.com/docs/api/authentication
         const requestURL = svc.getURL() + endpoint
         const svcCredential = svc.getConfiguration().credential;
@@ -44,58 +45,43 @@ function getOperationService() {
             .setRequestMethod("POST")
             .addHeader('Authorization', 'Bearer ' + svcCredential.user)
             .setURL(requestURL);
-        return JSON.stringify(request);
+        make(svc)
     })
 }
 
-function createCustomer(cc_customer_id) {
+function createCustomer(name, email, customerNo) {
     // https://stripe.com/docs/api/customers/create
-    const service = getOperationService()
-    const res = service.call("/customers", {
-        metadata: {
-            cc_customer_id
-        }
+    const service = _getOperationService()
+    const res = service.call("/customers", function (svc) {
+        svc.addParam('name', name);
+        svc.addParam('email', email);
+        svc.addParam('metadata[cc_customer_no]', customerNo);
     })
     return res.object;
 }
 
-function createPaymentIntent(amount, currency) {
+function createPaymentIntent(amount, currency, paymentMethodId, customerId) {
     // https://stripe.com/docs/api/payment_intents/create
-    const service = getOperationService()
-    const res = service.call("/payment_intents", {
-        amount,
-        currency
-    })
+    const service = _getOperationService()
+    const res = service.call("/payment_intents",
+        function (svc) {
+            svc.addParam('amount', amount * 100);
+            svc.addParam('currency', currency);
+            svc.addParam('payment_method', paymentMethodId);
+            svc.addParam('customer', customerId);
+        })
     return res.object;
 }
 
-function updatePaymentIntent(id, payment_method) {
-    // https://stripe.com/docs/api/payment_intents/update
-    const service = getOperationService()
-    const res = service.call(`/payment_intents/${id}`, {
-        payment_method
-    })
-    return res.object;
-}
-
-function confirmPaymentIntent(id) {
-    // https://stripe.com/docs/api/payment_intents/update
-    const service = getOperationService()
-    const res = service.call(`/payment_intents/${id}/confirm`, {})
-    return res.object;
-}
-
-function listPaymentMethods() {
+function listPaymentMethods(customerId) {
     // https://stripe.com/docs/api/payment_methods/list
-    const service = getRetrieveServise()
-    const res = service.call("/payment_methods")
+    const service = _getRetrieveServise()
+    const res = service.call(`/payment_methods?customer=${customerId}`)
     return res.object;
 }
 
 module.exports = {
     createCustomer,
     createPaymentIntent,
-    updatePaymentIntent,
-    listPaymentMethods,
-    confirmPaymentIntent
+    listPaymentMethods
 };
